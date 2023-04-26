14d0758bea6302b7f6576d046b21474428bf0ccf
Andre Soares <aersoares81@users.noreply.github.com>
2023-04-26 17:06:21 +0200

    Update plotting_PCA.R

--- plotting_PCA.R
+++ plotting_PCA.R
@@ -21,7 +21,8 @@ colnames(samplemat) <- sampleheaders
 # Extract the eigenvectors and turn them into a dataframe for plotting
 eigenvectors = mme.pca$vectors #extract eigenvectors 
 # Combining the information from the populations/samples
-pca.vectors = as_tibble(cbind(samplemat, data.frame(eigenvectors))) 
+pca.vectors = as_tibble(cbind(samplemat, data.frame(eigenvectors)))
+# You can edit this too
 legend_title <- "Location"
 
 # This will create a "simple" PCA plot. It will have a legend with the location of the samples next to it.
@@ -30,26 +31,26 @@ legend_title <- "Location"
 # Uncomment and use the palette your like the most if you want to change the colors.
 # Don't forget to uncomment the library in in the beginning of the script too.
 simple.pca = ggplot(data = pca.vectors, aes(x=X1, y=X2, colour = location, label = sample_id)) + 
-geom_point() + 
-labs(title="Homo sapiens", x="PC1",y="PC2",fill = "Location", colour ="Location", labels="Location") + 
-theme(plot.title = element_text(face = "italic")) #+ scale_color_brewer(palette="Set1")
+  geom_point() + 
+  labs(title="Homo sapiens", x="PC1",y="PC2",fill = "Location", colour ="Location", labels="Location") + 
+  theme(plot.title = element_text(face = "italic")) #+ scale_color_brewer(palette="Set1")
 plot(simple.pca)
 
 # This will create another PCA plot.
 # In this version, every point (or as much as possible considering their density) will have the sample ID assigned to it.
 # It's neat if you want to see where exactly each sample is placed.
 labeled.pca = ggplot(data = pca.vectors, aes(x=X1, y=X2, colour = location, label = sample_id)) + 
-geom_point() + labs(title="Homo sapiens", x="PC1",y="PC2",fill = "Location", colour ="Location", labels="Location") + 
-theme(plot.title = element_text(face = "italic")) + 
-geom_label_repel(show.legend = FALSE) #+ scale_color_brewer(palette="Set1")
+  geom_point() + labs(title="Homo sapiens", x="PC1",y="PC2",fill = "Location", colour ="Location", labels="Location") + 
+  theme(plot.title = element_text(face = "italic")) + 
+  geom_label_repel(show.legend = FALSE) #+ scale_color_brewer(palette="Set1")
 plot(labeled.pca)
 
 # This will output a PDF file with your plot, adjust the size and dpi to your liking.
 #It will be created in your basedir.
 ggsave(filename = paste0(basedir, "simple_pca_plot.pdf"), 
-plot = simple.pca, width = 28, height = 24, units = "cm", dpi = 600) 
+  plot = simple.pca, width = 28, height = 24, units = "cm", dpi = 600) 
 
 # This will output a PDF with plot that has all the labels.
 # It will be created in your basedir.
 ggsave(filename = paste0(basedir, "labeled_pca_plot.pdf"), 
-plot = labeled.pca, width = 28, height = 24, units = "cm", dpi = 600)
+  plot = labeled.pca, width = 28, height = 24, units = "cm", dpi = 600)
